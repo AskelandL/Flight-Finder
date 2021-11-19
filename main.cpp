172a8ee2a2a7510e73539959cd3095aad4aae3a0
Morgan Askeland <61073367+AskelandL@users.noreply.github.com>
2021-11-19 15:54:22 -0500

    added codependencies

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#include "EdgeList.h"
+#include "AdjacencyMatrix.h"
+#include "Random.h"
+#include "Display.h"
 // main file where everything is tied together : leave until we have function signiatures for everything else
 
 int main() {
