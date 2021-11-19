0c0ee8f408e4ac87f67f53cd33ebeadd786d8428
Morgan Askeland <61073367+AskelandL@users.noreply.github.com>
2021-11-19 15:29:10 -0500

    added outline to graph header files

---
+++ EdgeList.h
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class EdgeList {
+    // private variables etc
+
+public:
+    class Iterator;
+    void insertEdge(int from, int to, int weight);
+    vector<int> getAdjacent(int vertex);
+
+    class Iterator{
+        // private variables etc
+    public:
+        Iterator(const EdgeList& graph);
+        Iterator& operator++();
+    };
+};
