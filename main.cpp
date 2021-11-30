ba4abfa799184156fa0c0e3cd7664a6e3e7de9cd
Morgan Askeland <61073367+AskelandL@users.noreply.github.com>
2021-11-30 15:27:14 -0500

    added some files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
-#include "EdgeList.h"
 #include "AdjacencyMatrix.h"
 #include "Random.h"
 #include "Display.h"
-// main file where everything is tied together : leave until we have function signiatures for everything else
+// main file where everything is tied together
 
 int main() {
-    //
+    // create instance
+    // go through loop NODE times and insert node
+    // call Display?
 }
