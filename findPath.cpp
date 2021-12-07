6e611fc1ea52a95ca30f591736cfbead39179314
Tristan <tristan.mclain@ufl.edu>
2021-12-07 15:16:10 -0500

    updated headers

--- findPath.cpp
+++ findPath.cpp
@@ -1,24 +1,28 @@
-#include "findPath.h"
+#pragma once
+#include <vector>
+#include <set>
+#include "AdjacencyMatrix.h"
 
 int Dijkstra(AdjacencyMatrix& graph, int from, int to) {
 	// declare vectors and sets
 	set<int> visited;
 	set<int> notVisited;
-	vector<int> distance(graph.numVert, 9999999999);
-	vector<int> previous(graph.numVert, -1);
+	vector<int> distance(graph.getVertices(), 9999999999);
+	vector<int> previous(graph.getVertices(), -1);
 
 	visited.insert(from);
 	distance[from] = 0;
-	for (int i = 0; i < graph.numVert; i++) {
+	for (int i = 0; i < graph.getVertices(); i++) {
 		if (i != from) {
 			notVisited.insert(i);
 		}
 	}
 
 	for (auto v : notVisited) {
-		for (int i = 0; i < graph.myMatrix[from].size(); i++) {
-			if (graph.myMatrix[from][i] == v) {
-				distance[v] = graph.myMatrix[from][i];
+		for (int i = 0; i < graph.getMatrix()[from].size(); i++) {
+			if (graph.getMatrix()[from][i] == v) {
+				int tempDist = graph.getMatrix()[from][i];
+				distance[v] = tempDist;
 				previous[v] = from;
 				break;
 			}
@@ -39,10 +43,10 @@ int Dijkstra(AdjacencyMatrix& graph, int from, int to) {
 		visited.insert(minVertex);
 
 		for (auto v : notVisited) {
-			for (int i = 0; i < graph.myMatrix[minVertex].size(); i++) {
-				if (graph.myMatrix[minVertex][i] == v) {
-					if (distance[minVertex] + graph.myMatrix[minVertex][i] < distance[v]) {
-						distance[v] = distance[minVertex] + graph.myMatrix[minVertex][i];
+			for (int i = 0; i < graph.getMatrix()[minVertex].size(); i++) {
+				if (graph.getMatrix()[minVertex][i] == v) {
+					if (distance[minVertex] + graph.getMatrix()[minVertex][i] < distance[v]) {
+						distance[v] = distance[minVertex] + graph.getMatrix()[minVertex][i];
 						previous[v] = minVertex;
 					}
 					break;
@@ -54,21 +58,20 @@ int Dijkstra(AdjacencyMatrix& graph, int from, int to) {
 	return distance[to];
 }
 int BellmanFord(AdjacencyMatrix& graph, int from, int to) {
-	vector<int> distance(graph.numVert, 9999999999);
-	vector<int> previous(graph.numVert, -1);
+	vector<int> distance(graph.getVertices(), 9999999999);
+	vector<int> previous(graph.getVertices(), -1);
 	distance[from] = 0;
 	previous[from] = NULL;
 
-	for (int i = 0; i < graph.numVert; i++) {
-		for (int j = 0; j < graph.myMatrix[i].size(); j++) {
-			if (graph.myMatrix[i][j] != 0) {
-				int tempDist = distance[j] + graph.myMatrix[i][j];
+	for (int i = 0; i < graph.getVertices(); i++) {
+		for (int j = 0; j < graph.getMatrix()[i].size(); j++) {
+			if (graph.getMatrix()[i][j] != 0) {
+				int tempDist = distance[j] + graph.getMatrix()[i][j];
 				if (tempDist < distance[i]) {
 					distance[i] = tempDist;
 					previous[i] = j;
 				}
 			}
-
 		}
 	}
 
